a1577a822a4c8c1246c337393ead568b08d27420
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-28 05:31:33 +0300

    Init

--- src/jest.config.ts
+++ src/jest.config.ts
@@ -2,7 +2,10 @@ import type { Config } from 'jest';
 
 const config: Config = {
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/src/', '<rootDir>/test/'],
+  modulePathIgnorePatterns: [],
+  moduleNameMapper: {
+    '@service/(.+)': '<rootDir>/dist-test/service/$1',
+  },
 };
 
 export default config;
---
+++ test/main.ts
@@ -0,0 +1,13 @@
+import http from 'http';
+import { port } from '@service/port.js';
+import { hostname } from '@service/hostname.js';
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
